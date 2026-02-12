551bc7dc11fa4ede7d82a34dda469d861aafc58b
karamakcsc <karamakcsc@hotmail.com>
2026-02-12 15:51:03 +0300

    Update

--- masar_livestock/masar_livestock/doctype/letter_of_credit/letter_of_credit.py
+++ masar_livestock/masar_livestock/doctype/letter_of_credit/letter_of_credit.py
@@ -6,8 +6,9 @@ from frappe.model.document import Document
 
 
 class LetterofCredit(Document):
-	def validate(self):
-		if self.purchase_order:
-			exists = frappe.db.exists(self.doctype, {"purchase_order": self.purchase_order}, "name")
-			if exists:
-				frappe.throw(f"The Linked PO: {self.purchase_order} already has an existing Letter of Credit: {exists}")
+    pass
+	# def validate(self):
+	# 	if self.purchase_order:
+	# 		exists = frappe.db.exists(self.doctype, {"purchase_order": self.purchase_order}, "name")
+	# 		if exists:
+	# 			frappe.throw(f"The Linked PO: {self.purchase_order} already has an existing Letter of Credit: {exists}")
