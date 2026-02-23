7a0948ae3015f29f67f37eda70b5a4c2c32e3c60
karamakcsc <karamakcsc@hotmail.com>
2026-02-23 12:25:07 +0300

    Feed Cost

--- masar_livestock/custom/delivery_note/delivery_note.py
+++ masar_livestock/custom/delivery_note/delivery_note.py
@@ -1,6 +1,6 @@
 import frappe
 from masar_livestock.utilites import validate_livestock_items, set_weight_sle_sabb, reverse_weight_sle_sabb
-
+##
 
 def validate(self, method):
     validate_livestock_items(self)
