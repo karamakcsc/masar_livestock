c75de55fb505c31bd5de7e188bd04168528ffe60
karamakcsc <karamakcsc@hotmail.com>
2026-03-01 13:19:37 +0300

    Update

--- masar_livestock/custom/stock_entry/stock_entry.py
+++ masar_livestock/custom/stock_entry/stock_entry.py
@@ -1,7 +1,7 @@
 import frappe
 from masar_livestock.utilites import validate_livestock_items, set_weight_sle_sabb, reverse_weight_sle_sabb
 
-
+##
 def validate(self, method):
     validate_livestock_items(self)
 def on_submit(self, method):
